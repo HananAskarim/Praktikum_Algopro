889aa7332bbf038cde92b8ac937e2dc39531c5f2
L200180170 <44084593+L200180170@users.noreply.github.com>
2018-12-22 20:47:37 +0700

    Add files via upload

---
+++ Praktikum12 kegiatan2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+print "<!DOCTYPE html>"
+print
+print """<html>
+<head><title>Selamat Datang</title></head>
+<body>
+<table>
+	<tr>
+		<td widht='100' align='center' rowspan='6'>FOTO</td>
+		<td><b><font size='5'>Data diri</font><b></td>
+	</tr>
+	<tr>
+		<td>Nama </td>
+		<td>:</td>
+		<td>Hanan Askarim</td>
+	</tr>
+	<tr>
+		<td>Alamat tinggal </td>
+		<td>:</td>
+		<td>Kedawung, Sragen</td>
+	</tr>
+	<tr>
+		<td>Tempat, tanggal lahir </td>
+		<td>:</td>
+		<td>Sragen, 12 Juli 1999</td>
+	</tr>
+	<tr>
+		<td>Tempat wisata favorit </td>
+		<td>:</td>
+		<td>Yang bisa membuat kesenangan</td>
+	</tr>
+	<tr>
+		<td>Motto <td>
+		<td>:</td>
+		<td>jangan lelah untuk selalu berusaha</td>
+	</tr>
+</table>
+</body>
+</html>"""
